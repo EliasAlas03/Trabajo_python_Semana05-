c70425638e878d444bb457fe097fc243008e4e67
EliasAlas03 <126420750+EliasAlas03@users.noreply.github.com>
2023-02-26 15:10:40 -0600

    Add files via upload

---
+++ Python/actividadAsincrona – Semana05(GrupoNumero) py.py
@@ -0,0 +1,34 @@
+print ("Hola ing. Carballo somos los estudiantes de la clase de programación")
+print ("Nombres de los integrantres")
+print ("")
+a = "integrante 1"
+print (a)
+nombreUno = " Juan "
+nombreDos = " Elias "
+apellidoUno = " Alas "
+apellidoDos = " Artiga "
+print (nombreUno, nombreDos, apellidoUno, apellidoDos)
+s= "Sexo: Masculino"
+print  (s)
+e ="Edad: 19"
+print (e)
+edadUno= 19
+egresar= 5
+edadEgresar=edadUno + egresar
+print ("La edad de Elias Alas al egresar de la carrera de ingenieria sera de : ", edadEgresar), print ("años")
+
+
+print ("")
+print ("ESTO ES UNA ASIGNACIÓN DE CADENAS DE CARACTERES")
+mensaje= "Hola soy Elias Alas"
+buscarSudCadena= mensaje.find ("Elias")
+print (buscarSudCadena)
+print ("")
+
+print ("El promedio de la edad de los integrantes del grupo es : ")
+edadUno= 19
+edadDos= 18
+dividiendo= 5
+resultado= edadUno + edadDos
+print ("Suma de edades : ", resultado)
+print ("Promedio de edades : ", resultado / dividiendo)
